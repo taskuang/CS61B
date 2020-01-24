e60666057b33617a1d0e5dcc308601c20a782fac
Tasman Kuang <taskuang@berkeley.edu>
2020-01-24 10:45:34 -0800

    Completed Year.java

--- lab1/Year.java
+++ lab1/Year.java
@@ -5,8 +5,8 @@ public class Year {
 
     /** Return true iff YEAR is a leap year.  */
     static boolean isLeapYear(int year) {
-        if ((year % 400 == 0) || (year % 4 == 0 && year % 100)){
-          return true
+        if (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)){
+          return true;
         }
         return false;    // TODO: YOUR CODE HERE
     }
