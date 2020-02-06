952d058ec1408995b148f26b5f68e69411e883d4
Tasman Kuang <taskuang@berkeley.edu>
2020-02-06 01:22:16 -0800

    Edited CompoundInterest.java

--- hw1/CompoundInterest/CompoundInterest.java
+++ hw1/CompoundInterest/CompoundInterest.java
@@ -11,7 +11,7 @@ public class CompoundInterest {
      *  should be 1. Throughout the assignment it is OK to assume that
      *  TARGETYEAR is >= THIS_YEAR. */
     static int numYears(int targetYear) {
-        return 0;
+        return targetYear - THIS_YEAR;
     }
 
     /** Suppose we have an asset worth PRESENTVALUE that appreciates
@@ -23,7 +23,10 @@ public class CompoundInterest {
      *  then the futureValue will be 10*1.12*1.12 = 12.544. */
     static double futureValue(double presentValue, double rate,
                               int targetYear) {
-        return 0;
+        if (targetYear - THIS_YEAR == 0){
+            return presentValue;
+        }
+        return futureValue(presentValue * (1 + rate/100), rate, targetYear - 1);
     }
 
     /** Returns returns the value, in THIS_YEAR dollars, of an asset
@@ -37,7 +40,9 @@ public class CompoundInterest {
      *  2020 dollars, we get 12.544 * 0.97 * 0.97 = 11.8026496 dollars. */
     static double futureValueReal(double presentValue, double rate,
                                   int targetYear, double inflationRate) {
-        return 0;
+        if (targetYear == THIS_YEAR)
+            return presentValue;
+        return futureValueReal(presentValue * (1 + rate/100) * (1 - inflationRate/100), rate, targetYear - 1, inflationRate);
     }
 
     /** Suppose you invest PERYEAR dollars at the end of every year until
@@ -48,7 +53,16 @@ public class CompoundInterest {
      *  then the result will be 5000*1.1*1.1 + 5000*1.1 + 5000 =
      *  16550. */
     static double totalSavings(double perYear, int targetYear, double rate) {
-        return 0;
+        double savings = 0;
+        double simpRate = 1.0 + rate/100;
+        for (int i = 0; i <= targetYear - THIS_YEAR; i++){
+            double temp = perYear;
+            for (int j = 0; j < i; j++){
+                temp *= simpRate;
+            }
+            savings += temp;
+        }
+        return savings;
     }
 
     /** Returns totalSavings(PERYEAR, TARGETYEAR, RATE) converted to
@@ -56,7 +70,12 @@ public class CompoundInterest {
      *  INFLATIONRATE. */
     static double totalSavingsReal(double perYear, int targetYear, double rate,
                                    double inflationRate) {
-        return 0;
+        double savings = totalSavings(perYear, targetYear, rate);
+        double simpRate = 1.0 - inflationRate/100;
+        for (int i = 0; i < targetYear - THIS_YEAR; i++){
+            savings *= simpRate;
+        }
+        return savings;
     }
 
     /** Prints out the future inflation-adjusted value of a dollar in
@@ -65,8 +84,8 @@ public class CompoundInterest {
      *  INFLATIONRATE. */
     static void printDollarFV(int targetYear, double returnRate,
                               double inflationRate) {
-        double nominalDollarValue = 0; // replace 0 with your code
-        double realDollarValue = 0;    // replace 0 with your code
+        double nominalDollarValue = futureValue(1.00, returnRate, targetYear); // replace 0 with your code
+        double realDollarValue = futureValueReal(1.00, returnRate, targetYear, inflationRate);    // replace 0 with your code
 
         // Do not change anything in this method below this line
         String dollarSummary =
@@ -85,8 +104,8 @@ public class CompoundInterest {
     static void printSavingsFV(int targetYear, double returnRate,
                                double inflationRate, double perYear) {
 
-        double nominalSavings = 0; // replace 0 with your code
-        double realSavings = 0;    // replace 0 with your code
+        double nominalSavings = totalSavings(perYear, targetYear, returnRate); // replace 0 with your code
+        double realSavings = totalSavingsReal(perYear, targetYear, returnRate, inflationRate);    // replace 0 with your code
 
         // Do not change anything in this method below this line
 
--- hw1/CompoundInterest/CompoundInterestTest.java
+++ hw1/CompoundInterest/CompoundInterestTest.java
@@ -5,30 +5,41 @@ public class CompoundInterestTest {
 
     @Test
     public void testNumYears() {
-        /** Sample assert statement for comparing integers.
 
-        assertEquals(0, 0); */
+        /** Sample assert statement for comparing integers.
+         *
+         */
+        assertEquals(0, CompoundInterest.numYears(2020));
+        assertEquals(1, CompoundInterest.numYears(2021));
     }
 
     @Test
     public void testFutureValue() {
         double tolerance = 0.01;
+        assertEquals(7.744, CompoundInterest.futureValue(10, -12, 2022), tolerance);
+        assertEquals(12.544, CompoundInterest.futureValue(10, 12, 2022), tolerance);
     }
 
     @Test
     public void testFutureValueReal() {
         double tolerance = 0.01;
+        assertEquals(11.8026496, CompoundInterest.futureValueReal(10,12,2022,3),tolerance);
+        assertEquals(7.2863296,CompoundInterest.futureValueReal(10,-12,2022,3),tolerance);;
+
     }
 
 
     @Test
     public void testTotalSavings() {
         double tolerance = 0.01;
+        assertEquals(16550, CompoundInterest.totalSavings(5000,2022,10),tolerance);
+
     }
 
     @Test
     public void testTotalSavingsReal() {
         double tolerance = 0.01;
+        assertEquals(15571.895, CompoundInterest.totalSavingsReal(5000,2022,10 ,3), tolerance);
     }
 
 
