b601f09303363e043ad545fd87cefa70025a5f98
Tasman Kuang <taskuang@berkeley.edu>
2020-02-07 00:28:57 -0800

    Edited MultiArr

--- hw1/MultiArr/MultiArr.java
+++ hw1/MultiArr/MultiArr.java
@@ -15,6 +15,12 @@ public class MultiArr {
     */
     public static void printRowAndCol(int[][] arr) {
         //TODO: Your code here!
+        int column = 0;
+        for (int i = 0; i < arr.length; i ++){
+            if (arr[i].length > column)
+                column = arr[i].length;
+        }
+        System.out.println("Rows: " + arr.length + "\nColumns: " + column);
     } 
 
     /**
@@ -23,13 +29,27 @@ public class MultiArr {
     */
     public static int maxValue(int[][] arr) {
         //TODO: Your code here!
-        return 0;
+        int value = 0;
+        for (int i = 0; i < arr.length; i++){
+            for (int j = 0; j < arr[i].length; j++){
+                if (arr[i][j] > value){
+                    value = arr[i][j];
+                }
+            }
+        }
+        return value;
     }
 
     /**Return an array where each element is the sum of the 
     corresponding row of the 2d array*/
     public static int[] allRowSums(int[][] arr) {
         //TODO: Your code here!!
-        return null;
+        int[] sums = new int[arr.length];
+        for (int i = 0; i < arr.length; i++){
+            for (int j = 0; j < arr[i].length; j++){
+                sums[i] += arr[i][j];
+            }
+        }
+        return sums;
     }
 }
--- hw1/MultiArr/MultiArrTest.java
+++ hw1/MultiArr/MultiArrTest.java
@@ -6,11 +6,17 @@ public class MultiArrTest {
     @Test
     public void testMaxValue() {
         //TODO: Your code here!
+        int[][] testArr = new int[3][2];
+        testArr[2][1] = 2;
+        assertEquals(2, MultiArr.maxValue(testArr));
     }
 
     @Test
     public void testAllRowSums() {
         //TODO: Your code here!
+        int[][] testArr = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int[] expected = new int[]{6, 15, 24};
+        assertArrayEquals(expected, MultiArr.allRowSums(testArr));
     }
 
 
